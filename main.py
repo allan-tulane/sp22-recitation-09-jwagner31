128577f705ac32835cf04dea843227c43091a38c
jwagner31 <70210002+jwagner31@users.noreply.github.com>
2022-05-07 21:30:36 +0000

    first question done

--- main.py
+++ main.py
@@ -2,19 +2,39 @@ from collections import deque
 from heapq import heappush, heappop 
 
 def shortest_shortest_path(graph, source):
-    """
-    Params: 
-      graph.....a graph represented as a dict where each key is a vertex
-                and the value is a set of (vertex, weight) tuples (as in the test case)
-      source....the source node
-      
-    Returns:
-      a dict where each key is a vertex and the value is a tuple of
-      (shortest path weight, shortest path number of edges). See test case for example.
-    """
-    ### TODO
-    pass
+  """
+  Params: 
+    graph.....a graph represented as a dict where each key is a vertex
+              and the value is a set of (vertex, weight) tuples (as in the test case)
+    source....the source node
     
+  Returns:
+    a dict where each key is a vertex and the value is a tuple of
+    (shortest path weight, shortest path number of edges). See test case for example.
+  """
+  ### TODO
+  frontier = [(0, source)]
+  depth = {}
+  weight = {}
+  for node in graph:
+    depth[node] = 999
+    weight[node] = 999
+  depth[source] = 0
+  weight[source] = 0
+  while frontier:
+    dep, key = heappop(frontier)
+    #Go through the neighbors of what we just popped
+    for neigh, neighWeight in graph[key]:
+      if depth[neigh]>dep+1 or depth[neigh]>=998:
+        heappush(frontier, (dep+1, neigh))
+        depth[neigh] = dep+1
+        weight[neigh] = neighWeight + weight[key]
+  result = {}
+  for node in graph:
+    result[node] = (weight[node], depth[node])
+  return result
+  
+      
 def test_shortest_shortest_path():
 
     graph = {
